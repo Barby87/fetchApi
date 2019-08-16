d8c31021d4df850191d4e6c17a2db7de1e27f79a
Barby87 <bcaceres.sepulveda@hotmail.com>
2019-08-16 19:26:02 -0400

    Consumiendo datos desde api

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var contenido = document.querySelector('#contenido');
 
 function traer() {
     //Dentro del fetch va la url que queremos ocupar
-    fetch('texto.txt')
-    .then(data => data.text()) // La primera promeza trae los datos y los transforma en texto (formato que se puede leer) 
-    .then(data =>{ // La segunda promesa trae los datos ya transformados y los pinta en consola y en el navegador.
-        console.log(data);
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json()) // La primera promesa trae los datos y los transforma en texto (formato que se puede leer) 
+    .then(data => { // La segunda promesa trae los datos ya transformados y los pinta en consola y en el navegador.
+        console.log(data.results[0])
         //Creando contenido html con comillas especiales, a través de js
-        contenido.innerHTML= `${data}`;
+        contenido.innerHTML= `<img src="${data.results[0].picture.large}" width="100px" class="img-fluid rounded-circle">
+        <p>Hello! My name is ${data.results[0].name.first}</p>
+        <p>Cell: ${data.results[0].cell}</p>
+        <p>e-mail: ${data.results[0].email}</p>`;
     })
-
-   
 }
+
